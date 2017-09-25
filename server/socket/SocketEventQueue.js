c322235654b2d3b0262847d3d8bb1cfd94267bb2
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2017-09-25 08:40:30 -0400

    Add SocketEventQueue for consuming socket evnts with order guarantees

---
+++ server/socket/SocketEventQueue.js
@@ -0,0 +1,97 @@
+import _ from "lodash";
+/*
+type ClientEventQueue {
+    queuedEvents: {},
+    maxSeq: number,
+    pendingSeq: number[],
+}
+*/
+
+/**
+ * Manages incoming socket events by client to ensure that messages from a given
+ * client are processed in sending order. Assumes that all events received from
+ * the client have a 'seq' attribute on them that is numeic and strictly
+ * ascending
+ */
+
+export default class SocketEventQueue {
+  constructor(EVENT_QUEUE_LIMIT) {
+    this.queuedEvents = [];
+    this.maxSeq = 0;
+    this.pendingSeq = [];
+    this.queuedSeq = [];
+  }
+
+  ingestEvent(event) {
+    if (!Number.isInteger(event.seq)) {
+      throw new Error(`event.seq (${event.seq}) is not a number`);
+    }
+    if (!event.seq >= 1) {
+      throw new Error(`event.seq (${event.seq}) is not >= 1`);
+    }
+    // Remove clients who are performing socket-queueing attacks on the server
+    if (event.seq - this.maxSeq > this.EVENT_QUEUE_LIMIT) {
+      throw new Error(`Client missing > ${this.EVENT_QUEUE_LIMIT} events`);
+    }
+
+    // If the pending queue is empty and the event is the next expected
+    // one, send it immediately
+    if (this.queuedEvents.length === 0 && this.maxSeq + 1 === event.seq) {
+      this.__sendEvent(event);
+      this.maxSeq++;
+      return;
+    }
+
+    let pending_seq_queue_index = this.pendingSeq.indexOf(event.seq);
+    if (pending_seq_queue_index !== -1) {
+      this.pendingSeq.splice(pending_seq_queue_index, 1);
+    } else if (event.seq <= this.maxSeq) {
+      throw new Error(
+        "Ingested non-pending event with seq ${event.seq}. Duplicate event?"
+      );
+    }
+
+    // Queue the message & update maxSeq / pendingQueue
+    this.__addEventToQueue(event);
+
+    // If pendingSeq is empty, send all messages in the queuedMessages.
+    if (this.pendingSeq.length === 0) {
+      // Empty the event queue
+      let events_to_send = this.queuedEvents;
+      this.queuedEvents = [];
+      this.queuedSeq = [];
+      // Send all events in the queue
+      // TODO yield context while sending to allow intake of new events &
+      // processng client events
+      for (let event of events_to_send) {
+        this.__sendEvent(event);
+      }
+    }
+  }
+
+  /**
+   * Insert an event into an event queue maintaining ordering by index
+   */
+  __addEventToQueue(event) {
+    let i = 0;
+    while (
+      i < this.queuedEvents.length &&
+      this.queuedEvents[i].seq < event.seq
+    ) {
+      i++;
+    }
+    this.queuedEvents.splice(i, 0, event);
+    this.queuedSeq.push(event.seq);
+    let new_pending = _.range(Math.max(this.maxSeq, 1), event.seq).filter(
+      i => this.queuedSeq.indexOf(i) === -1
+    );
+    this.pendingSeq = this.pendingSeq.concat(new_pending);
+    this.maxSeq = Math.max(this.maxSeq, event.seq);
+  }
+
+  __sendEvent(event) {
+    if (this.onSendEvent) {
+      this.onSendEvent(event);
+    }
+  }
+}
---
+++ server/socket/__tests__/SocketEventQueueTest.js
@@ -0,0 +1,79 @@
+import SocketEventQueue from "../SocketEventQueue";
+
+describe("SocketEventQueue", () => {
+  let queue;
+  let sent_events;
+
+  beforeEach(() => {
+    queue = new SocketEventQueue(400);
+    sent_events = [];
+    queue.onSendEvent = event => sent_events.push(event);
+  });
+
+  it("throws an error when receiving an event without a seq", () => {
+    expect(() => queue.ingestEvent({})).toThrow();
+  });
+
+  it("throws an error when receiving an event with a non-numeric seq", () => {
+    expect(() => queue.ingestEvent({ seq: "one" })).toThrow();
+    expect(() => queue.ingestEvent({ seq: "1" })).toThrow();
+    expect(() => queue.ingestEvent({ seq: {} })).toThrow();
+  });
+
+  it("throws an error when receiving an event with a negative seq", () => {
+    expect(() => queue.ingestEvent({ seq: -1 })).toThrow();
+    expect(() => queue.ingestEvent({ seq: 0 })).toThrow();
+  });
+
+  it("sends events immediately when receiving them in sequence", () => {
+    let evt1 = { seq: 1, payload: "What's up" };
+    let evt2 = { seq: 2, payload: "It's Ya Boi" };
+    let evt3 = { seq: 3, payload: "Frank" };
+    queue.ingestEvent(evt1);
+    expect(sent_events).toEqual([evt1]);
+    queue.ingestEvent(evt2);
+    expect(sent_events).toEqual([evt1, evt2]);
+    queue.ingestEvent(evt3);
+    expect(sent_events).toEqual([evt1, evt2, evt3]);
+  });
+
+  it("throws an error when receiving an event multiple times", () => {
+    let evt1 = { seq: 1, payload: "Hey Guys, sorry I haven't been updating" };
+    queue.ingestEvent(evt1);
+    expect(sent_events).toEqual([evt1]);
+    expect(() => queue.ingestEvent(evt1)).toThrow();
+  });
+
+  it("throws an error when the client queues too many events", () => {
+    // Queue events up to the limit, skipping seq 1 so that the ingested
+    // events are queued instead of sent
+    for (let i = 1; i < 400; i++) {
+      queue.ingestEvent({ seq: i + 1 });
+    }
+
+    // Queue events up to the limit, skipping 0
+    expect(() =>
+      queue.ingestEvent({
+        seq: 400
+      })
+    ).toThrow();
+  });
+
+  it("Sends queued events in seq order when sequence has been received", () => {
+    let evt1 = { seq: 1 };
+    let evt2 = { seq: 2 };
+    let evt3 = { seq: 3 };
+    let evt4 = { seq: 4 };
+    queue.ingestEvent(evt3);
+    expect(sent_events).toEqual(
+      [],
+      "No event should be send when pending messages exist"
+    );
+    queue.ingestEvent(evt2);
+    expect(sent_events).toEqual([]);
+    queue.ingestEvent(evt4);
+    expect(sent_events).toEqual([]);
+    queue.ingestEvent(evt1);
+    expect(sent_events).toEqual([evt1, evt2, evt3, evt4]);
+  });
+});
