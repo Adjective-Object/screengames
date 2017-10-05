d67550e98960f5efae90bf7d19a5260eea3f8bcb
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2017-10-04 22:33:17 -0400

    clone data deep in dataProvider

--- util/test/dataProvider.js
+++ util/test/dataProvider.js
@@ -1,6 +1,7 @@
+import { cloneDeep } from "lodash";
 const dataProvider = dataset => test_function => {
   for (let test_name in dataset) {
-    let test_data = dataset[test_name];
+    let test_data = cloneDeep(dataset[test_name]);
     it(test_name, () => test_function(test_data));
   }
 };
