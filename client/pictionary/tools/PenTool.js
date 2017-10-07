d66d692c2dfe8025eac1a21d84c2ef5a75f6895f
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2017-10-06 22:09:05 -0400

    move distance function into common utility

--- client/pictionary/tools/PenTool.js
+++ client/pictionary/tools/PenTool.js
@@ -1,8 +1,5 @@
 import guid from "../../util/guid";
-
-function distance(p1, p2) {
-  return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
-}
+import distance from "../../util/distance";
 
 /*
 interface Point {
