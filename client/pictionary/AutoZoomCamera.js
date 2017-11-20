77dca566822f567dac2690127b523b56a65e83ee
Maxwell Huang-Hobbs <mhuangh@fb.com>
2017-11-20 12:09:51 -0500

    use BoundTracker in AutoZoomCamera

--- client/pictionary/AutoZoomCamera.js
+++ client/pictionary/AutoZoomCamera.js
@@ -1,99 +1,37 @@
 // @flow
 import { transform, identity, translate, scale } from 'transformation-matrix';
+import transformFromCameraBounds from '../util/transform-from-camera-bounds';
 import PointSpace from './PointSpace';
+import BoundTracker from './BoundTracker';
 import type { Matrix } from 'transformation-matrix';
 import type { Event, EventConsumer } from './EventConsumer';
 import type { Point } from './Drawing';
 import type { ICamera } from './Camera';
 
-type CameraTransformEvent = {
-  type: 'adjust_transform',
-  transform: Matrix,
-};
-
-type CameraCenterCanvasEvent = {
-  type: 'center_canvas',
-};
-
-export type Events = CameraTransformEvent | CameraCenterCanvasEvent;
-
 export default class AutoZoomCamera implements ICamera, EventConsumer {
   drawTarget: HTMLSvgElement;
   margin: number;
-  x: number;
-  y: number;
-  width: number;
-  height: number;
+  boundTracker: BoundTracker;
 
   constructor(margin: number, draw_target: HTMLSvgElement) {
     this.margin = margin;
-    this.x = 0;
-    this.y = 0;
-    this.width = 1;
-    this.height = 1;
     this.drawTarget = draw_target;
+    this.boundTracker = new BoundTracker();
   }
 
   ingestEvent(event: Event): boolean {
-    switch (event.type) {
-      case 'initialize':
-        for (let strokeID in event.initial_state.strokes) {
-          let stroke = event.initial_state.strokes[strokeID];
-          for (let point of stroke.points) {
-            this.adjustBounding(point);
-          }
-        }
-        return true;
-      case 'append_stroke':
-      case 'add_stroke':
-        this.adjustBounding(event.point);
-        return true;
-      case 'clear_canvas':
-        this.x = 0;
-        this.y = 0;
-        this.width = 1;
-        this.height = 1;
-        return true;
-      default:
-        return false;
-    }
+    return this.boundTracker.ingestEvent(event);
   }
 
   canIngestEvent(event: Event) {
-    const allowed_events = [
-      'initialize',
-      'append_stroke',
-      'add_stroke',
-      'clear_canvas',
-    ];
-    return allowed_events.indexOf(event.type) !== -1;
+    return this.boundTracker.canIngestEvent(event);
   }
 
-  getTransform(): Matrix {
-    let scale_ratio = Math.min(
-      this.drawTarget.viewBox.baseVal.width / this.width,
-      this.drawTarget.viewBox.baseVal.height / this.height,
-      1,
+  getTransform() {
+    return transformFromCameraBounds(
+      this.boundTracker.getBounds(),
+      this.margin,
+      this.drawTarget,
     );
-    let margin_ratio = 1 + this.margin * 2;
-    let trans = transform(
-      scale(scale_ratio, scale_ratio),
-      translate(-this.x, -this.y),
-      scale(1 / margin_ratio, 1 / margin_ratio),
-      translate(
-        this.margin * this.drawTarget.viewBox.baseVal.width / 2,
-        this.margin * this.drawTarget.viewBox.baseVal.height / 2,
-      ),
-    );
-    return trans;
-  }
-
-  adjustBounding(point: Point): void {
-    let new_x = Math.min(this.x, point.x);
-    let new_y = Math.min(this.y, point.y);
-    this.width = Math.max(this.width - (new_x - this.x), point.x - this.x);
-    this.height = Math.max(this.height - (new_y - this.y), point.y - this.y);
-    this.x = new_x;
-    this.y = new_y;
   }
 }
