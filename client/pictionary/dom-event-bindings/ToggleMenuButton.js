496c7e2e44fba96e1a32915f296e7bcf2fa02344
Maxwell Huang-Hobbs <mhuangh@fb.com>
2017-12-03 21:37:46 -0500

    click overlay to hide sidebar

--- client/pictionary/dom-event-bindings/ToggleMenuButton.js
+++ client/pictionary/dom-event-bindings/ToggleMenuButton.js
@@ -3,10 +3,16 @@
 export default class ToggleMenuButton {
   menuElement: Element;
   buttonElement: Element;
+  overlayElement: Element;
 
-  constructor(menu_element: Element, button_element: Element) {
+  constructor(
+    menu_element: Element,
+    button_element: Element,
+    overlay_element: Element,
+  ) {
     this.menuElement = menu_element;
     this.buttonElement = button_element;
+    this.overlayElement = overlay_element;
   }
 
   close(): void {
@@ -22,8 +28,14 @@ export default class ToggleMenuButton {
   }
 
   bind() {
-    this.menuElement.addEventListener('click', () => {
+    this.buttonElement.addEventListener('click', () => {
+      console.log('toggle on button click');
       this.toggle();
     });
+    this.overlayElement.addEventListener('click', () => {
+      console.log('hide on overlay click');
+      this.close();
+    });
+    return this;
   }
 }
